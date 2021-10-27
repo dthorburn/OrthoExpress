248946d8729d99709e09c7ce22195b44a7d08bd1
dthorburn <35698678+dthorburn@users.noreply.github.com>
2021-10-27 16:51:43 +0100

    Update app.r

--- app.r
+++ app.r
@@ -9,11 +9,16 @@
 suppressMessages(library(data.table))
 suppressMessages(library(ggpattern))
 suppressMessages(library(ggplot2))
+suppressMessages(library(ggstance))
+suppressMessages(library(stringr))
+suppressMessages(library(ggtree))
 suppressMessages(library(dplyr))
 suppressMessages(library(shiny))
 
 ## Reading in the data and setting the directory
 #  params.directory <- "D:/Imperial/Projects/01_Neg_Dom_Allele/06_RNA_Seq"
+#  params.directory <- "C:/Users/Miles/Documents/Imperial/Projects/01_Neg_Dom_Allele/06_RNA_Seq"
+#  params.directory <- "C:/Users/Miles/Documents/Imperial/Projects/05_OrthoExpress/02_Orthology_Data/04_App_Data"
 #  setwd(params.directory)
 #  Rose_melted <- fread("Processed_Rose_RNASeq.csv")
 #  Taxi_melted <- fread("Processed_Taxiarchi_RNASeq.csv")
@@ -22,11 +27,15 @@ suppressMessages(library(shiny))
 #  Pirbright_melted <- fread("Processed_Pirbright_RNASeq.csv")
 
 ## Saving all data for ease of use
-#save.image(file = "All_Expression_Dat_211021.RData")
+#save.image(file = "All_Expression_Dat_271021.RData")
+#save.image(file = "All_Orthology_Dat_261021.RData")
 
 ## loading in data
 #load(file = "All_Expression_Dat_201021.RData")
-load(file = "All_Expression_Dat_211021.RData")
+load(file = "All_Expression_Dat_271021.RData")
+load(file = "All_Orthology_Dat_261021.RData")
+load(file = "AgamP4_GO_Dataset_271021.RData")
+load(file = "All_Species_Tree_271021.RData")
 
 ## Defining the plotting functions - this is simply to make the plots more intuitive, 
 ## but can be reduced to make the tool run more smoothly if needed. 
@@ -53,7 +62,7 @@ Taxiarchi_Gene <- function(temp_gene){
       geom_bar(stat = "identity", fill = "slategrey", colour = "black", width = 1) +
       theme_classic(base_size = 20) +
       labs(x = "Meiotic Population", y = "FPKM", 
-        title = paste0("Taxiarchi dataset: ", temp_gene, "\nTestes Specific: ", ifelse(unique(temp_dat$Testes_Specific) == -1, "No", "Yes"), "; Ovaries Specific: ", ifelse(unique(temp_dat$Ovaries_Specific) == -1, "No", "Yes"))) +
+        title = paste0("Taxiarchi dataset: ", temp_gene)) +
       scale_x_discrete(expand = c(0,0)) +
       scale_y_continuous(expand = c(0,0)) 
   } else {
@@ -103,6 +112,79 @@ Papa_Gene <- function(temp_gene, include_oviposit = 0){
   }
 } 
 
+## The Orthology Query Tools
+## Part of the handling of multiple OGs from a single gene ID
+Multiple_Hits_OG <- function(row_num){
+  temp_OG <- dat[as.numeric(row_num),] %>% str_split(pattern = ":? ") %>% unlist
+  temp_OG[1] %>% return
+}
+## Defining the function to parse the OrthoFinder output. 
+Orthology <- function(query, input_feature = "Gene"){
+  ## Parameters for handling errors and debugging
+  Orthology_Errors <- ""
+  move_to_next <- 1
+
+  ## Handling the input feature types, so can use transcript and protein IDs
+  if(input_feature == "Protein"){
+    query_gene <- query
+  } else if(input_feature == "Transcript"){
+    query <- gsub(query, pattern = "([A-Z])(\\.[0-9]$)", replacement = "\\1")
+    query_gene <- Gene_Locs[grepl(pattern = query, Transcript_ID), Protein_ID]
+  } else if(input_feature == "Gene"){
+    ## This handles the potential outcome of a gene having proteins in multiple different orthogroups
+    query_gene <- Gene_Locs[grepl(pattern = query, Gene_ID), Protein_ID]
+    if(length(query_gene) >= 2){
+      OGs <- ""
+      for(i in 1:length(query_gene)){
+        OGs[i] <- grep(pattern = query_gene[i], dat$V1)
+      }
+      if(length(unique(OGs)) > 1){
+        Mult_OGs <- sapply(FUN = Multiple_Hits_OG, unique(OGs)) %>% as.vector
+        temp_orths <- lapply(FUN = Orthology, Mult_OGs) %>% rbindlist
+        move_to_next <- 0
+      }
+    }
+  }
+
+  if(move_to_next == 1){
+    query_row <- grep(pattern = query_gene, dat$V1)
+    ## Handling the output if no/multiple orthogroups are identified - which I'm unsure if the latter is even possible.
+    if(length(query_row) == 0){
+      Orthology_Errors <- paste0(Orthology_Errors, "\nERROR: No results found for ", query_gene, ", please check query and try again\nIf the alternative IDs work, but not this feature, please report this error to d.thorburn@imperial.ac.uk")
+    } else  if(length(query_row) >= 2){
+      ## Handing the potential outcome of a protein being in multiple OGs, then it recursively calls each OG and returns them all in 1 data.table
+      Mult_OGs <- sapply(FUN = Multiple_Hits_OG, unique(OGs)) %>% as.vector
+      temp_orths <- lapply(FUN = Orthology, Mult_OGs) %>% rbindlist
+      Orthology_Errors <- paste0(Orthology_Errors, "\nWARN: ", query_gene, " is in ", length(unique(OGs)), " orthogroups\n")
+      if(str_count(Orthology_Errors) > 0){
+        message(Orthology_Errors)
+      }
+      return(temp_orths)
+    } else  if(length(query_row) == 1){
+      ## This was a little tricky - I had to use capture groups to NOT take the ".1" from anything starting with .1
+      temp_pattern <- dat[query_row,] %>% str_split(pattern = ":? ") %>% unlist %>% gsub(., pattern = "([A-Z])(\\.[0-9]$)", replacement = "\\1")
+      temp_orths <- Gene_Locs[Gene_Locs$Protein_ID %in% temp_pattern[2:length(temp_pattern)],] %>% unique
+      
+      ## Just in case this is still problematic and some proteins are not assigned correctly
+      if(length(temp_pattern[!temp_pattern %in% temp_orths$Protein_ID]) != 1){
+        Orthology_Errors <<- paste0(Orthology_Errors, "\nERROR: Not all proteins found for ", query_gene, "\nPlease report this error to d.thorburn@imperial.ac.uk\n")
+      }
+      temp_orths[,"Orthogroup_ID" := temp_pattern[1]]
+      if(str_count(Orthology_Errors) > 0){
+        message(Orthology_Errors)
+      }
+      return(temp_orths)
+    }
+  } else if(move_to_next == 0){
+    Orthology_Errors <- paste0(Orthology_Errors, "\nWARN: ", query, " has proteins in ", length(unique(OGs)), " orthogroups\n")
+    if(str_count(Orthology_Errors) > 0){
+      message(Orthology_Errors)
+    }
+    return(temp_orths)
+  }
+}
+
+
 ## UI Settings
 ui <- pageWithSidebar(
   # Application title
@@ -110,9 +192,16 @@ ui <- pageWithSidebar(
 
   ## The options section
   sidebarPanel(
+    width = 3,
     textInput("gene_id", h3("Query Gene:"), value = ""),
-    helpText("Use VectorBase gene IDs (e.g., AGAP001094).", br(), 
-              "I'm working on implementing using ortholog IDs too, but this will take some time."),
+    helpText("For expression plots use ", em("Anopheles gambiae")," VectorBase gene IDs (e.g., AGAP001094). Transcript and protein IDs and orthologous sequences is not currently implemented.", br(), br(), "For orthology assignment select annotation feature below 
+      and use VectorBase or Ensembl IDs for any of the species included (e.g., ", em("Drosophila melanogaster")," transcript FBtr0077192). See More Info tab for species information. "),
+    radioButtons("annGroup", h3("Annotation Feature"),
+                              choices = list("Gene" = "Gene", "Transcript" = "Transcript", "Protien" = "Protein"), selected = "Gene"),
+    submitButton("Submit Query"),
+    br(),
+    downloadButton("downloadData", "Download Orthology Table"),
+    downloadButton("downloadGOData", "Download Gene Ontology Table"),
     checkboxGroupInput("checkGroup", h3("Expression Datasets:"), 
                               choices = list("Papa et al. 2017" = "Papa",
                                              "Unpublished Pirbright" = "Pirbright",
@@ -121,32 +210,49 @@ ui <- pageWithSidebar(
                                              "Rose et al. 2016" = "Rose"
                                              ),
                               selected = 0),
-    helpText("For more information on expression units or the naming conventions used see the \"More Info\" tab."),
+    helpText("Select up to 5 datasets to plot. For more information on expression units or references see the \"More Info\" tab."),
 
     radioButtons("oviGroup", h3("Papa dataset, include oviposit columns?"),
                               choices = list("Yes" = 1, "No" = 0), selected = 0),
-    submitButton("Select Expression Data"),
-    helpText("To report bugs or request data to be added, please contact Miles Thorburn <d.thorburn@imperial.ac.uk>")
+    helpText(br(), "To report bugs or request data to be added, please contact Miles Thorburn <d.thorburn@imperial.ac.uk>")
   ),
 
   ## Defining the output and panels
   mainPanel(
     tabsetPanel(
-      tabPanel("Expression Datasets", 
+      
+      tabPanel("Expression Plots", 
         plotOutput("plot1"),
         plotOutput("plot2"),
-        plotOutput("plot3"),
         plotOutput("plot4"),
         plotOutput("plot5")
       ),
-      tabPanel("Orthology", 
       
+      tabPanel("Orthology Table", 
+        dataTableOutput("Orth_Tab")
       ),
+      
+      tabPanel("Orthology Plot", 
+        plotOutput("Orth_Plot", height = "850px"),
+      ),
+      
+      tabPanel("Gene Ontology", 
+        dataTableOutput("GOs_Tab")
+      ),
+      
       tabPanel("More Info",
-        h2("Phylogenetic Species Tree Based on Orthology:"),
-        h4("OrthoFinder (Emms & Kelly, 2019) was used to infer the species tree based on an all-versus-all peptide comparison among all insect species. Here, colours represent different orders: Blue is Diptera; Green is Lepidoptera; Yellow is Hymenoptera; and Red is Coleoptera."),
-        HTML('<center><img src="Consensus_Species_Tree_OrthoFinder.png" width="480" height="660"></center>'),
+        h2("How it works:"),
+        h4(strong("Expression: "), "The first step is ensuring the query is either an ", em("Anopheles gambiae"), " gene, or that the query has only 1 ", em("A. gambiae"),
+        "ortholog. Then the requested datasets are simply subset to only include that data and plotted in an informative manner."),
+        h4(strong("Orthology: "), "Orthology was assigned using OrthoFinder (Emms & Kelly, 2019). OrthoFinder infers orthogroups (both orthology and paralogy) for all species based on an 
+        all-versus-all peptide comparison. It also creates gene trees for all orthogroups and infers a species tree (see below).",
+        "This tool first converts the query into the protein accession, then finds the associated orthogroup and returns that table. If more than one orthogroup
+        is identified (i.e., a gene with multiple transcripts in different orthogroups), the function will recursively run through each orthogroup."),
         br(),
+        
+        h2("Known Bugs:"),
+        h4("You cannot change to the Orthology Table tab before you've submitted a query"),
+        hr(),
 
         h2("Expression Units:"),
         h4(strong("TPM: Transcript Per Million."), " This is calculated following this logic, 
@@ -155,7 +261,7 @@ ui <- pageWithSidebar(
         h4(strong("FPKM: Fragments Per Kilobase of transcript per Million reads mapped."), " Whereas RPKM was developed for single-end RNAseq analyses, FPKM uses paired-end sequencing. Here, FPKM also accounts for whether both reads mapped to the same trascript"),
         h4(strong("RMA: Robust Multiarray Averaging."), " A method of normalising microarray probe intesnities, and includes background correction, normalization, perfect match correction and summarization with the RMA algorithm."),
         br(),
-               
+
         h2("References:"),
         h4(strong("Baker, D. A., Nolan, T., Fischer, B., Pinder, A., Crisanti, A. and Russell, S."), 
           " (2011). Female-biased gene expression in the malaria mosquito Anopheles gambiae. BMC Genomics 12, 1–12. doi: 10.1016/j.cub.2005.03.005."),
@@ -163,21 +269,29 @@ ui <- pageWithSidebar(
           " (2017). Rapid evolution of female-biased genes among four species of Anopheles malaria mosquitoes. Genome Research 27, 1536–1548. doi: 10.1101/gr.217216.116."),
         h4(strong("Rose, G., Krzywinska, E., Kim, J., Revuelta, L., Ferretti, L. and Krzywinski, J."), 
           "  (2016). Dosage compensation in the African malaria mosquito Anopheles gambiae. Genome Biology and Evolution 8, 411–425. doi: 10.1093/gbe/evw004."),
+        h4(strong("Emms, D. M. and Kelly, S."), 
+          " (2019). OrthoFinder: Phylogenetic orthology inference for comparative genomics. Genome Biology 20, 1–14. doi: 10.1101/466201."),
         h4(strong("Taxiarchi, C., Kranjc, N., Kriezis, A., Kyrou, K., Bernardini, F., Russell, S., Nolan, T., Crisanti, A. and Galizi, R."), 
           " (2019). High-resolution transcriptional profiling of Anopheles gambiae spermatogenesis reveals mechanisms of sex chromosome regulation. Scientific Reports 9, 1–12. doi: 10.1038/s41598-019-51181-1."),
         h4(strong("Pirbright Institute"), 
-          " (Unpublished). ", a("Data Set", href = "https://vectorbase.org/vectorbase/app/record/dataset/DS_bf500a6707"), ": Developmental transcriptome of sexed Anopheles gambiae larvae and adult mosquitoes group 2")
+          " (Unpublished). ", a("Data Set", href = "https://vectorbase.org/vectorbase/app/record/dataset/DS_bf500a6707"), ": Developmental transcriptome of sexed Anopheles gambiae larvae and adult mosquitoes group 2. [Downloaded from VectorBase; 10/2021]")
       ),
-      tabPanel("Debugging", textOutput("txt"))
+#      tabPanel("Debugging", 
+#        textOutput("input"),
+#        textOutput("txt"),
+#        textOutput("Exp_Errs"),
+#        textOutput("Orth_Errs")
+#      )
     )
   )
 )
+#shinyApp(ui, server)
 
 ## Server Logic
 server <- function(input, output) {
+  #shinyAppDir(".")
+  errors <- ""
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Expression output  
-  shinyAppDir(".")
-  
   ## Defining the parameters and removing any white spaces in the input
   gene_id <- reactive({
     gene_id <- input$gene_id %>% trimws
@@ -228,13 +342,88 @@ server <- function(input, output) {
       eval(parse(text = string))
     }
   })
-  ## The Help Box 
-  output$txt <- renderText({
-    ## Defining the datasets to use for expression data
-    data_names <- input$checkGroup
-      paste0("Datasets: ", paste(data_names, collapse = ", "), "\n", input$gene_id)
-  })
+
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Orthology output  
+  Orth_dat <- reactive({
+    string <- paste0("Orthology(\"", gene_id(), "\", input_feature = \"", input$annGroup, "\")")
+    out_tab <- eval(parse(text = string))
+    out_tab
+  })
+#  Orth_errors <- reactive({
+#    string <- paste0("Orthology(\"", gene_id(), "\", input_feature = \"", input$annGroup, "\")")
+#    out_errs <- capture.output(eval(parse(text = string)), type = "message")
+#  })
+  output$Orth_Tab <- renderDataTable({
+    Orth_dat()
+  })
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(gene_id(), "_Orthology.csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(Orth_dat(), file, row.names = FALSE)
+    }
+  )
+
+  output$Orth_Plot <- renderPlot({    
+    Orths <- Orth_dat()
+    Orth_tab <- table(Orths$Species) %>% as.data.table 
+    names(Orth_tab) <- c("Full_Name", "Num_Orthologs")
+    Orth_tab[,"Full_Name" := ifelse(grepl(pattern = "Aedes_aegypti_lvpagwg", Full_Name), "Aedes_aegypti", Full_Name)]
+
+    ## This awfulness is the simplest solution for acutally getting this damn thing to work. Using the same column name in different
+    ## dfs just kept causing errors, despite actually working for a bit when testing. 
+    Metadat2 <- merge(Metadat1, Orth_tab, all = TRUE, by = "Full_Name")[, union(names(Metadat1), names(Orth_tab))] 
+    Metadat2$Num_Orthologs <- ifelse(is.na(Metadat2$Num_Orthologs), 0, Metadat2$Num_Orthologs)
+    Metadat2$Order2 <- Metadat2$Order %>% as.factor
+
+    tre <-  MT  %<+% Metadat1 +
+        geom_tiplab(aes(colour = Order, label=paste0('italic(', genus, ')~italic(', species,')')), parse=TRUE, hjust = 0, show.legend = F, align=TRUE, linesize=.5, size = 6) +
+        geom_text2(aes(label=label, subset = !is.na(as.numeric(label)) & as.numeric(label) > 50), hjust = -0.1, colour = "darkgrey", size = 3.5) +
+        scale_colour_manual(values = c("red", "blue", "darkorange", "darkgreen")) 
+    tre2 <- facet_plot(tre  + theme_tree2(), panel = 'Number of Orthologs', 
+                  data = Metadat2, geom = geom_barh,
+                  mapping = aes(x = Num_Orthologs, fill = Order2),
+                  stat='identity', colour = "black", width =1) +
+        scale_x_continuous(expand = c(0,0), limits =c(0,28)) +
+        scale_fill_manual(values = c("red", "blue", "darkorange", "darkgreen")) + 
+        labs(fill = "Phylogenetic Order") + theme(legend.position = "bottom") +
+        theme(axis.text = element_text(size = 20), strip.text.x = element_text(size = 20), 
+                  legend.text = element_text(size = 20), legend.title = element_text(size = 25)) 
+    facet_plot(tre2, data = Metadat2, panel = "Number of Orthologs", geom = geom_text, 
+                  mapping = aes(x = Num_Orthologs + 0.4, label = Num_Orthologs), size = 6, vjust = 0.45)
+  })
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GO output  
+  GOs_dat <- reactive({
+    dat <- subset(GOs, Gene_ID == gene_id())
+    setkey(dat, GO_Domain, GO_Term)
+    dat
+  })
+  output$GOs_Tab <- renderDataTable({
+    GOs_dat()
+  })
+    output$downloadGOData <- downloadHandler(
+    filename = function() {
+      paste(gene_id(), "_GOs.csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(GOs_dat(), file, row.names = FALSE)
+    }
+  )
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Debugging output
+#  output$input <- renderText({
+#    paste0("Query: ", paste(gene_id(), input$annGroup, sep = "; "))
+#  })
+#  output$txt <- renderText({
+#    data_names <- input$checkGroup
+#    paste0("Expression Datasets: ", paste(data_names, collapse = ", "))
+#  })
+#  output$Exp_Errs <- renderText({
+#    paste0("Expression Warnings: ", paste(errors))
+#  })
+#  output$Orth_Errs <- renderText({
+#    #paste0("Orthology Warnings: ", paste(Orth_errors()))
+#  })
 }
-shinyApp(ui, server)
+shinyApp(ui, server)  
